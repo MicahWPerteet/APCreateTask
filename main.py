36a07c09f1f11d1cd9f2abd3da7913614e1ad1b8
TheSnackle <lightbluebabyfox@gmail.com>
2025-10-06 13:03:00 -0500

    created the rough draft of the gambling math and chances

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # --- IMPORTS ---
 import pygame
 import pygame_menu
+import random as rand
+import numpy as np
+
 
 # --- INITS ---
 pygame.init()
@@ -60,3 +63,61 @@ while game_running:
     CLOCK.tick(FPS)
 
 pygame.quit()
+
+
+# Gambling shenanganery, Will inplement once chances are done
+
+
+icons = ["7", "bell", "clover", "cherry", "triple bar", "double bar", "bar"]
+num1= None
+num2= None
+num3= None
+
+mean = 0
+std_dev = 1
+
+bar = .4
+bar2 = .8
+bar3 = 1.2
+cherry = 1.6
+clover = 2
+bell = 2.4
+
+
+def spin():
+    global num1, num2, num3
+    num1 = np.random.normal(loc=mean, scale=std_dev)
+    num2 = np.random.normal(loc=mean, scale=std_dev)
+    num3 = np.random.normal(loc=mean, scale=std_dev)
+def rank(num):
+    if abs(num) <= bar:
+        num = icons[6]
+    elif abs(num) <= bar2:
+        num = icons[5]
+    elif abs(num) <= bar3:
+        num = icons[4]
+    elif abs(num) <= cherry:
+        num = icons[3]
+    elif abs(num) <= clover:
+        num = icons[2]
+    elif abs(num) <= bell:
+        num = icons[1]
+    else:
+        num = icons[0]    
+    return(num)
+def roll():
+    global num1, num2, num3, icons
+    spin()
+    num1 = rank(num1)
+    num2 = rank(num2)
+    num3 = rank(num3)
+    print("rolling...")
+    output = [num1, num2, num3]
+    print(output)
+    if output[0] == output[1] and output[1] == output [2]:
+        print("Full", num2, "straight")
+    elif output[0] == output[1] or output[1] == output[2]: 
+        print(num2, "straight")
+
+for i in range(5):
+    roll()
